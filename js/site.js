21e91ac4529b75bd4663289b336b1db1a2f57562
Jason Wicker <jmwick01@gmail.com>
2024-03-05 07:34:42 -0500

    Updated HTML with project specific information. Started adding JavaScript.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World");    
+
+
+
+//get the values from the page
+//start or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //call generateNumbers
+    let numbers = generateNumbers()
+    //call displayNumbers
+}
+
+//generate numbers from the start value to the end value
+//logic funtion(s)
+function generateNumbers(sValue, eValue) {
+    
+    let numbers = [];
+    return numbers;
+
+}
+
+//display the numbers and mark even numbers bold
+//display or view function
+function displayNumbers() {
+
 }
