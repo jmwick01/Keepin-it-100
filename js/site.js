ac540bb1ce2ae72038c3ccaacbf8d25cdd0f46b4
Jason Wicker <jmwick01@gmail.com>
2024-03-11 21:54:23 -0400

    Updated JS
    Added JS functionality to display numbers in a row and display the even numbers in bold.

--- js/site.js
+++ js/site.js
@@ -8,9 +8,22 @@ function getValues() {
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
-    //call generateNumbers
-    let numbers = generateNumbers()
-    //call displayNumbers
+    //we need to validate our input
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter integers");        
+    }
+
+    
+    
 }
 
 //generate numbers from the start value to the end value
@@ -18,12 +31,34 @@ function getValues() {
 function generateNumbers(sValue, eValue) {
     
     let numbers = [];
+
+    //we want to get all numbers from start to end
+    for (let index = sValue; index <= eValue; index++) {
+
+        //this will execute in a loop until index = eValue
+        numbers.push(index);
+        
+    }
     return numbers;
 
 }
 
 //display the numbers and mark even numbers bold
 //display or view function
-function displayNumbers() {
+function displayNumbers(numbers) {
 
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+        let className = "even";
+        let number = numbers[index];
+
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
